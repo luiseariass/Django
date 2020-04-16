5f8b3dab668a1072b58af481456cfccbe85f507a
luiseariass <luisearias94@gmail.com>
2020-04-16 16:22:03 -0500

    Actualizando

--- proyecto1/proyecto1/view.py
+++ proyecto1/proyecto1/view.py
@@ -2,44 +2,61 @@ import os
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+
+class Persona(object):
+	
+	def __init__(self, nombre,apellido):
+		
+		self.nombre = nombre
+		self.apellido = apellido
+
 def saludo(request):
-	script_dir = os.path.dirname(__file__) 
-	rel_path = "/Plantillas/primera.html"
-	abs_file_path = os.path.join(script_dir, rel_path)
-	doc_externo=open(abs_file_path)
-	plant=Template(doc_externo.read())
-	doc_externo.close()
-	cnt=Context()
-	documento= plant.render(cnt)
-	return HttpResponse(documento)
+    # Entrar al directorio de plantillas
+    script_dir = os.path.dirname(__file__)
+    rel_path = "../Plantillas/primera.html"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    doc_externo = open(abs_file_path)
+    plant = Template(doc_externo.read())
+    doc_externo.close()
+    p1= Persona("Luis Enrique","Arias Serrano")
+    ahora = datetime.datetime.now()
+    #temas=["Plantillas","Modelos","Formulario","Vistas","Aplicaciones"]
+    temas=[]
+    cnt = Context({'Nombre_programador': p1.nombre,
+                   'Apellido_programador': p1.apellido,'Ahora':ahora,"Temas":temas})
+    
+    documento = plant.render(cnt)
+
+    return HttpResponse(documento)
+
 
 def despedida(request):
 
-	return HttpResponse('Chao mundo')
+    return HttpResponse('Chao mundo')
+
 
 def damefecha(request):
-	fecha_actual=datetime.datetime.now()
-	documento="""<html>
+    fecha_actual = datetime.datetime.now()
+    documento = """<html>
 	<body>
 	<h2>
 	Fecha y hora actual %s
 	</h2>
 	</body>
 	</html>""" % fecha_actual
-	return(HttpResponse(documento))
+    return(HttpResponse(documento))
 
-	
+
 def calculaedad(request, agno):
 
-	edadactual=25;
-	periodo= agno-2020
-	EdadFutura=edadactual+periodo
-	documento="""<html>
+    edadactual = 25
+    periodo = agno - 2020
+    EdadFutura = edadactual + periodo
+    documento = """<html>
 	<body>
 	<h2>
 	En el año %s tendras %s años
 	</h2>
 	</body>
-	</html>""" %(agno,EdadFutura)
-	return(HttpResponse(documento))
-
+	</html>""" % (agno, EdadFutura)
+    return(HttpResponse(documento))
